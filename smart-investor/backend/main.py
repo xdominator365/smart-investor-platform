ddf4161d63fd84e20d4e291cfef8fdaccdea4831
Yadav <harshit.b.yadav@accenture.com>
2026-08-14 00:08:59 +0530

    fix

--- smart-investor/backend/main.py
+++ smart-investor/backend/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://dhira-fv9909tn3-xdominator365s-projects.vercel.app/"
+        "https://dhira-fv9909tn3-xdominator365s-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
